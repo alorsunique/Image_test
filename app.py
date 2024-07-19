9f45973389d4f5e503f700ad382f4fbd33f44204
alorsunique <sounique7800@gmail.com>
2024-07-19 23:31:57 +0800

    2024 Jul 19

--- app.py
+++ app.py
@@ -1,4 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
+from pathlib import Path
 
 app = Flask(__name__)
 
+
+image_list = []
+
+@app.route('/')
+def home():
+    return render_template('index.html', image_list=image_list)
+
+
+image_folder_directory = Path("D:\Projects\Recovery\Recovered\LOONA")
+
+
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return send_from_directory(image_folder_directory, filename)
+
+if __name__ == "__main__":
+
+    print("Hello World")
+
+    
+
+    for file in image_folder_directory.iterdir():
+        print(file)
+        image_list.append(file.name)
+
+    
+
+
+
+    app.run(debug=True)
