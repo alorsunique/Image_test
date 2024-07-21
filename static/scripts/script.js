9458d9978e82a625810438dfa80ba7f8d28b7c71
alorsunique <sounique7800@gmail.com>
2024-07-21 22:14:59 +0800

    2024 Jul 21

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -5,5 +5,28 @@ function bruhMoment() {
     alert("Bruh Moment");
 }
 
+function note_image() {
+    const image_box = document.getElementsByClassName("row-image");
+    //console.log(image_box);
+    console.log("Here")
+    return image_box
+}
+
+document.addEventListener("DOMContentLoaded", note_image);
+
+console.log(image_box)
+
+
+
+
+image_box.array.forEach(element => {
+    element.addEventListener('click',bruhMoment)
+});
+
+
+//image_box[0].addEventListener("click", bruhMoment());
+
+//image_box.addEventListener("click", bruhMoment());
+
 //document.getElementsByClassName("row-image").addEventListener("click", bruhMoment());
 
